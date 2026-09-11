9b85bbb7d9499281422b2a38f8d6eb9e60a8e48f
KJyothiPriya <jyothipriyak1@gmail.com>
2026-09-11 12:58:08 +0530

    Create C_quotes.py
    Use Case 1

---
+++ C_quotes.py
@@ -0,0 +1,18 @@
+# 1. Using DOUBLE QUOTES " " 
+# Single quote ' inside unna problem ledu
+str1 = "This is Inceptez's \"Python\" class for Data Engineers & AI Engineers"
+print(str1)
+
+# 2. Using SINGLE QUOTES ' '
+# Double quotes " inside unna problem ledu, but single quote ki \ pettali
+str2 = 'This is Inceptez\'s "Python" class for Data Engineers & AI Engineers'
+print(str2)
+
+# 3. Using TRIPLE QUOTES """ """ or ''' '''
+# Triple quotes lo single & double rendu pettukovachu, escape avasaram ledu
+# Idi best method for this type sentence
+str3 = """This is Inceptez's "Python" class for Data Engineers & AI Engineers"""
+print(str3)
+
+str4 = '''This is Inceptez's "Python" class for Data Engineers & AI Engineers'''
+print(str4)
